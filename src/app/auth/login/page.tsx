78144f45297652e7a66b943eebd12549040f3031
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-12-28 14:57:58 -0600

    update

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,6 +5,8 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 
+
+
       <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Ingresar</h1>
 
       <div className="flex flex-col">
